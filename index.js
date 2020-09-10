aaa10b188c41db87ad74bc1897579a62327ed4b8
Brian Abeyta-Pratt <babeytapratt@gmail.com>
2020-09-10 00:53:14 -0600

    finished

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ console.log(votingAge)
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
 var age = 20;
-  console.log(age * 3);
+var drink = (age >= 21) ? "beer" : "Juice";
+
+console.log(drink);
 
 
 
@@ -22,6 +24,8 @@ var age = 20;
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 Number('1999');
+
+console.log(Number);
 
 
 
@@ -66,10 +70,48 @@ console.log(dogYears(39));
 // 7 - 12 months 4% of their body weight
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
-function dogFeeder(weight, age) {
 
+
+
+function dogFeeder(weightInPounds, ageInYears) {
+  let adultDog = 1
+  let puppy = (0-.9)
+
+    if (weightInPounds <= 5 && adultDog) {
+    return weightInPounds * .05;
+    } else if (weightInPounds > 5 <= 10 && adultDog) {
+    return weightInPounds * .04;
+    } else if (weightInPounds > 10 <= 15 && adultDog) {
+    return weightInPounds * .03;
+    } else if (weightInPounds > 15 && adultDog) {
+    return weightInPounds * .02;
+    } else if (weightInPounds < 5 && ageInYears >= .02 <= .04) {
+    return ageInYears * .1;
+    } else if (weightInPounds < 5 && ageInYears >= .05 <= .07) {
+    return ageInYears * .05;
+    } else if (weightInPounds < 5 && ageInYears >= .08 <= .09) {
+    return ageInYears * .04;
+    } else if (weightInPounds > 5 <= 10 && ageInYears >= .02 <= .04) {
+    return ageInYears * .1;
+    } else if (weightInPounds > 5 <= 10 && ageInYears >= .05 <= .07) {
+    return ageInYears * .05;
+    } else if (weightInPounds > 5 <= 10 && ageInYears >= .08 <= .09) {
+    return ageInYears * .04;
+    } else if (weightInPounds > 10 <= 15 && ageInYears >= .02 <= .04) {
+    return ageInYears * .1;
+    } else if (weightInPounds > 10 <= 15 && ageInYears >= .05 <= .07) {
+    return ageInYears * .05;
+    } else if (weightInPounds > 10 <= 15 && ageInYears >= .08 <= .09) {
+    return ageInYears * .04;
+    } else if (weightInPounds > 15 && ageInYears >= .02 <= .04) {
+    return ageInYears * .1;
+    } else if (weightInPounds > 15 && ageInYears >= .05 <= .07) {
+    return ageInYears * .05;
+    } else if (weightInPounds > 15 && ageInYears >= .08 <= .09) {
+    return ageInYears * .04;
 }
 
+console.log(dogFeeder(12,.07));
 
 
 
@@ -87,29 +129,29 @@ function dogFeeder(weight, age) {
 // if 2 computer will be paper
 // if 3 computer will be scissors
 // if computer choses rock, I choose
-Math.random()
-// inside function. 1 param
-var rock = 1
-var paper = 2
-var scissors = 3
+// Math.random()
+// // inside function. 1 param
+// var rock = 1
+// var paper = 2
+// var scissors = 3
 
-function RockPaperScissors(a,b,c) {
-    if { (1,2) {
-      console.log("You win!");
-    } else if { (1,3) {
-      console.log("You win!");
-    } else if { (2,1) {
-      console.log("You win!");
-     } else if {  (2,3) {
-      console.log("You lose!")
-    } else if {  (3,1) {
-      console.log("You lose!")
-    } else if {  (3,2) {
-      console.log("You win!")
-    }
-  }
+ function RockPaperScissors(input) {
+   let random = Math.floor(Math.random() * 3) + 1;
+   let compChoice;
+   let lowercaseInput = input.toLowerCase();
 
-  console.log(RockPaperScissors(rock, scissors));
+   if (random === 1)  {
+     compChoice = "rock";
+   }  else if (random === 2)  {
+     compChoice = "paper";
+   }  else if (random === 3)  {
+     compChoice = "scissors";
+   }
+
+   console.log(RockPaperScissors(1));
+
+
+
 
 
 
@@ -141,12 +183,14 @@ console.log(toCentimeters(3));
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-function annoyingSong (i);
-  for(let i = 100; i < function.length; i-- ) {
-    if (function[i] < 100) {
-      console.log([i] "bottles of beer on the wall," [i] "bottles of beer. Take one down, pass it around," [i] "bottles of beer on the wall!")
-    }
-  }
+ function annoyingSong (numberLeftOver)  {
+   for(let i = 100; i < annoyingSong.length; i-- ) {
+     if (annoyingSong[i] < 100) {
+       return ("[i]bottles of beer on the wall,[i]bottles of beer. Take one down, pass it around,[i]bottles of beer on the wall!");
+     }
+   }
+}
+
 console.log(annoyingSong(99));
 
 
@@ -160,6 +204,21 @@ console.log(annoyingSong(99));
 //60s should be D
 //and anything below 60 should be F
 
+function gradeCalculator(grade) {
+  if (grade < 60) {
+    return("Your grade is an F!");
+  } else if (grade >= 60 <= 69) {
+    return("Your grade is a D.");
+  } else if (grade >= 70 <= 79) {
+    return("Your grade is a C.");
+  } else if (grade >= 80 <= 89) {
+  return("Your grade is a B.");
+  } else if (grade >= 90 <= 100) {
+    return("Your grade is an A!");
+  }
+}
+
+console.log(gradeCalculator(76));
 
 
 
@@ -169,10 +228,20 @@ console.log(annoyingSong(99));
 // Hint - you may need to study tomorrow's traning kit on arrays
 // try looking up the .includes() method
 
+function countVowels (string) {
+  let count = 0;
+  let splitString = string.toLowerCase().split("");
 
+  splitString.forEach(v => {if (v === "a" || v === "e" || v === "i" || v === "o" || v === "u") {
+    count++;
+  }});
+  return count;
+ }
+
+console.log(countVowels("I LOVE LaMBda"));
 
 
 
 /************************************************************** Stretch **************************************************************/
 //Take Rock, Paper, Sissors further
-//update your rock papers sissors code below to take a prompt from a user using the window object
+// update your rock papers sissors code below to take a prompt from a user using the window object
